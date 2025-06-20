a7b464a07acd602f2212d4949af781d743f42423
kosyakeo <142715653+kosyakeo@users.noreply.github.com>
2025-06-20 21:39:37 +0700

    ohh shit

--- 6/1.py
+++
@@ -1,3 +0,0 @@
-names = ["Олег", "Архип", "Анна", "Даниил", "Стасик", "Влад", "Алиса", "Сергей", "Саня", "Ольга"]
-names_a = [name for name in names if name.startswith(('А', 'A'))]
-print(names_a)
--- 6/5.py
+++
@@ -1,4 +0,0 @@
-A = ["cat", "dog", "aurum", "brawlstars", "csgo", "glock", "cola", "calculator"]
-C = 'c'
-count = sum(1 for s in A if len(s) > 1 and s.startswith(C) and s.endswith(C))
-print(f"Количество подходящих элементов: {count}")
