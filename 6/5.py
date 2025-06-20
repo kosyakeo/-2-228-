a716f11ba9ec24f6427088c23b03cf91c4fd803c
kosyakeo <142715653+kosyakeo@users.noreply.github.com>
2025-06-20 21:29:36 +0700

    .

--- 6/5.py
+++ 6/5.py
@@ -1,4 +1,4 @@
-A = ["cat", "dog", "cucumber", "apple", "car", "clock", "cola", "calc"]
+A = ["cat", "dog", "aurum", "brawlstars", "csgo", "glock", "cola", "calculator"]
 C = 'c'
 count = sum(1 for s in A if len(s) > 1 and s.startswith(C) and s.endswith(C))
 print(f"Количество подходящих элементов: {count}")
